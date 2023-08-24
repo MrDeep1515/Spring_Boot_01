8f792e78fd3de62da67162e91181fe96a00bc614
MrDeep1515 <shindedeepak1515@gmail.com>
2023-08-24 12:44:18 +0530

    Change in productcontroller and productservice

--- src/main/java/com/rt/controller/Product_Controller.java
+++ src/main/java/com/rt/controller/Product_Controller.java
@@ -1,6 +1,11 @@
 package com.rt.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +24,23 @@ public class Product_Controller {
 
 			service.add(pe);
 		
-		return "Welcome To Spring Boot";
+			return "Product Added Successfully";
+	}
+	
+	@GetMapping("/add/id/{id}")
+	public Optional<Product_Entity> findProductById(@PathVariable int id) {
+
+		Optional<Product_Entity> pe = service.findProductById(id); 
+		
+		return pe;
+	}
+	
+	@DeleteMapping("/add/id/{id}")
+	public Product_Entity deleteProduct(@PathVariable int id) {
+
+		Product_Entity pe = service.deleteProductById(id);
+		
+		return pe;
 	}
 
 }
