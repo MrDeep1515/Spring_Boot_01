6e1040a3fabfafb14afd1d1b65ddd63abff34d79
MrDeep1515 <shindedeepak1515@gmail.com>
2023-08-25 12:49:09 +0530

    Add findAll method and findByProduct method in controller,service,dao.

--- src/main/java/com/rt/dao/Product_Dao.java
+++ src/main/java/com/rt/dao/Product_Dao.java
@@ -1,9 +1,17 @@
 package com.rt.dao;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rt.entity.Product_Entity;
 
 public interface Product_Dao extends JpaRepository<Product_Entity, Integer> {
 
+	List<Product_Entity> findByProduct(String product);
+
+	
+
 }
+
--- src/main/java/com/rt/service/Product_Service.java
+++ src/main/java/com/rt/service/Product_Service.java
@@ -1,5 +1,6 @@
 package com.rt.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,28 +11,42 @@ import com.rt.entity.Product_Entity;
 
 @Component
 public class Product_Service {
-	
+
 	@Autowired
 	Product_Dao dao;
 
 	public void add(Product_Entity pe) {
-		
-		dao.save(pe);
-		
-	}
-	
-	public Optional<Product_Entity> findProductById(int id) {
-		
-		  Optional<Product_Entity> pe = dao.findById(id);
-			
-			return pe;	
-		}
 
-		public Product_Entity deleteProductById(int id) {
-			
-			dao.deleteById(id);
-			
-			return null;
-		}
+		dao.save(pe);
+
+	}
+
+	public Optional<Product_Entity> findProductById(int id) {
+
+		Optional<Product_Entity> pe = dao.findById(id);
+
+		return pe;
+	}
+
+	public List<Product_Entity> findByProduct(String product) {
+		
+		List<Product_Entity> pe = dao.findByProduct(product);
+
+		return pe;
+	}
+
+	public Product_Entity deleteProductById(int id) {
+
+		dao.deleteById(id);
+
+		return null;
+	}
+
+	public List<Product_Entity> findAllProduct() {
+		List<Product_Entity> pe = dao.findAll();
+		return pe;
+	}
+
+	
 
 }
